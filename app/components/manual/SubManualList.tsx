983f8137802e7c3504fedc243e34f6f391eba67a
jiny_park <jiny3360@naver.com>
2023-06-28 14:31:34 +0900

    updated: tag style

--- app/components/manual/SubManualList.tsx
+++ app/components/manual/SubManualList.tsx
@@ -9,29 +9,42 @@ const SubManualList = (props: Props) => {
   if (props.type === "hsm") {
     return (
       <div>
-        <ul className="flex w-full justify-evenly ">
-          <li className="border-b-2 hover:border-black transition py-[10px] text-[16px] hover:font-bold w-[20%] text-center cursor-pointer">
-            <Link href='hsm/sa5'>SA5</Link>
-          </li>
-          <li className="border-b-2 hover:border-black transition py-[10px] text-[16px] hover:font-bold w-[20%] text-center cursor-pointer">
-            <Link href='hsm/sa7'>SA7</Link>
-          </li>
-          <li className="border-b-2 hover:border-black transition py-[10px] text-[16px] hover:font-bold w-[20%] text-center cursor-pointer">
-            <Link href='hsm/migration'>Migration</Link>
-          </li>
-          <li className="border-b-2 hover:border-black transition py-[10px] text-[16px] hover:font-bold w-[20%] text-center cursor-pointer">
-            <Link href='hsm/error'>Error</Link>
-          </li>
+        <ul className='flex w-full justify-evenly'>
+          <Link
+            className='border-b-2 hover:border-black transition py-[10px] text-[16px] hover:font-bold w-[20%] text-center cursor-pointer'
+            href='hsm/sa5'
+          >
+            SA5
+          </Link>
+
+          <Link
+            className='border-b-2 hover:border-black transition py-[10px] text-[16px] hover:font-bold w-[20%] text-center cursor-pointer'
+            href='hsm/sa7'
+          >
+            SA7
+          </Link>
+
+          <Link
+            className='border-b-2 hover:border-black transition py-[10px] text-[16px] hover:font-bold w-[20%] text-center cursor-pointer'
+            href='hsm/migration'
+          >
+            Migration
+          </Link>
+
+          <Link
+            className='border-b-2 hover:border-black transition py-[10px] text-[16px] hover:font-bold w-[20%] text-center cursor-pointer'
+            href='hsm/error'
+          >
+            Error
+          </Link>
         </ul>
       </div>
     );
   }
   if (props.type === "netapp") {
     return <div>SubManualList {props.type}</div>;
-  }
-
-  else {
-    return <></>
+  } else {
+    return <></>;
   }
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className='max-w-[1280px] mx-auto'>
       메인 페이지
-      <Link href="/about">about으로</Link>
+      <Link href='/about'>about으로</Link>
     </div>
-  )
+  );
 }
 
 
-const mainPageLayout = ''
+const mainPageLayout = "";
